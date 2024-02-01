6c87a713a501afbc7067e766935c05d2f48a1d01
cbdyzj <cbdyzj@gmail.com>
2024-02-01 13:56:31 +0000

    Deploying to gh-pages from @ nano-exp/nano-ui@5700d57bad3f1ee3b5b74f12c5585990f2fca1f1 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { default as a } from "./components/Button.js";
+import { default as a } from "./components/Button.vue.js";
 import { default as f } from "./components/Input.js";
 export {
   a as NButton,
