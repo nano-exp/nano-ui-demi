b2a6a7386a7eea9c0b1b22e1efb2dc79748b17d4
cbdyzj <cbdyzj@gmail.com>
2024-02-20 15:01:24 +0000

    Deploying to gh-pages from @ nano-exp/nano-ui@ff816ca7b9288033508df8fbaf9c9522c0fb6d3a 🚀

--- index.js
+++
@@ -1,8 +0,0 @@
-import { default as e } from "./components/Button.vue.js";
-import { default as f } from "./components/Tooltip.vue.js";
-import { default as l } from "./components/Flex.vue.js";
-export {
-  e as NButton,
-  l as NFlex,
-  f as NTooltip
-};
